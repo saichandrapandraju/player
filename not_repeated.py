816bdd461055f0ad49569d8adbd10e8f55f5ab3f
saichandrapandraju <41769919+saichandrapandraju@users.noreply.github.com>
2018-08-15 13:24:08 +0530

    Create not_repeated.py

---
+++ not_repeated.py
@@ -0,0 +1,16 @@
+g=input()
+c=len(g)
+q=0
+w=0
+new=[]
+list=[int(x) for x in input().split()]
+while(q==0):
+    l=list[q]
+    list.remove(list[0])
+    if((l in list)and(l not in new)):
+        new.insert(w,l)
+        w+=1
+    elif(l not in new):
+        print(l)
+    elif(list==[]):
+        break
